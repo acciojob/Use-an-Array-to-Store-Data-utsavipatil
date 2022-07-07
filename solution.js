6ec1a2115578a38aca3f08b6c651b4bad38facb8
Utsavi Patil <abc@gmail.com>
2022-07-07 17:40:15 +0530

    Array Initialize

--- solution.js
+++ solution.js
@@ -3,5 +3,6 @@ let yourArray;
 // assign your array the correct value in the next line
 // your code here!
 // code end
+yourArray = ["numbers" , 1 , true ,  false , 2 , 347.151]; 
 
 module.exports = yourArray
